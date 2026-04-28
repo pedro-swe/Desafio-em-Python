63315550b49917399f40631f3ec535f9675d9dc9
pedro-swe <phrj1671@gmail.com>
2026-04-28 19:23:14 -0300

    Add files via upload

---
+++ exx010.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n1 = float(input('Digite a 1° nota: '))
+n2 = float(input('Digite a 2° nota: '))
+m = (n1+n2)/2
+print('A média do aluno é {}'.format(m))
